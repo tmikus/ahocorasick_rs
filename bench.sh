d6f628d615d7915e7a5b9aa513de6dccf6df10b2
Tomasz Mikus <mikus.tomasz@gmail.com>
2023-08-24 08:45:57 +0100

    Change the CRLF files to LF.

--- bench.sh
+++ bench.sh
@@ -2,4 +2,4 @@
 
 (cd lib/ahocorasick_rs && cargo build --release)
 cp lib/ahocorasick_rs/target/release/libahocorasick_rs.so lib/
-go test -bench=. -ldflags="-r $(pwd)/lib"
+go test -bench=. -ldflags="-r $(pwd)/lib" -benchtime=20s
